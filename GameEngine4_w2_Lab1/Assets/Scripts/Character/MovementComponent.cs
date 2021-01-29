fca72f0c5015f8cc24976b8e05a075184c57822d
Talha991s <55731371+Talha991s@users.noreply.github.com>
2021-01-29 03:56:59 -0800

    Completed Lab1

--- GameEngine4_w2_Lab1/Assets/Scripts/Character/MovementComponent.cs
+++ GameEngine4_w2_Lab1/Assets/Scripts/Character/MovementComponent.cs
@@ -16,27 +16,58 @@ namespace Character
 
         //references
         private Transform PlayerTransform;
+        private Animator PlayerAnimator;
+        private Rigidbody PlayerRigidBody;
 
         private Vector2 InputVector = Vector2.zero;
         private Vector3 MoveDirection = Vector3.zero;
 
+        //Animator Hashes
+        public readonly int MovementXHash = Animator.StringToHash("MovementX");
+        public readonly int MovementYHash = Animator.StringToHash("MovementY");
+        public readonly int IsJumpingHash = Animator.StringToHash("IsJumping");
+        public readonly int IsRunningHash = Animator.StringToHash("IsRunning");
+
+
         private void Awake()
         {
             PlayerTransform = transform;
-            PlayerController = GetComponent<PlayerController>();  
+            PlayerController = GetComponent<PlayerController>();
+            PlayerAnimator = GetComponent<Animator>();
+            PlayerRigidBody = GetComponent<Rigidbody>();
         }
 
         public void OnMovement(InputValue value)
         {
             //Debug.Log(value.Get());
             InputVector = value.Get<Vector2>();
+            PlayerAnimator.SetFloat(MovementXHash, InputVector.x);
+            PlayerAnimator.SetFloat(MovementYHash, InputVector.y);
+
+        }
+
+        public void OnRun(InputValue value)
+        {
+            Debug.Log(value.isPressed);
+            PlayerController.IsRunning = value.isPressed;
+            PlayerAnimator.SetBool(IsRunningHash, value.isPressed);
+        }
+        public void OnJump(InputValue value)
+        {
+            PlayerController.IsJumping = value.isPressed;
+            PlayerAnimator.SetBool(IsJumpingHash, value.isPressed);
+
+            PlayerRigidBody.AddForce((PlayerTransform.up + MoveDirection) * JumpForce, ForceMode.Impulse);
         }
 
         private void Update()
         {
             if (PlayerController.IsJumping) return;
 
-            if (!(InputVector.magnitude > 0)) return;
+            if (!(InputVector.magnitude > 0))
+            {
+                MoveDirection = Vector3.zero;
+            }
 
             MoveDirection = (PlayerTransform.forward * InputVector.y) + (PlayerTransform.right * InputVector.x);
            
@@ -45,6 +76,14 @@ namespace Character
             Vector3 movementDirection = MoveDirection * (currentSpeed * Time.deltaTime);
 
             PlayerTransform.position += movementDirection;
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            if (!other.gameObject.CompareTag("Ground") && !PlayerController.IsJumping) return;
+            
+            PlayerController.IsJumping = false;
+            PlayerAnimator.SetBool(IsJumpingHash, false);
         }
     }
 }
