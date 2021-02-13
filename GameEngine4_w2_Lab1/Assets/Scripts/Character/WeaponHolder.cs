7e0fd40de204cc3cdc5408da009b698651e01bcb
Talha991s <55731371+Talha991s@users.noreply.github.com>
2021-02-13 11:48:22 -0800

    I don't know why I'm having an error

--- GameEngine4_w2_Lab1/Assets/Scripts/Character/WeaponHolder.cs
+++ GameEngine4_w2_Lab1/Assets/Scripts/Character/WeaponHolder.cs
@@ -1,24 +1,63 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using Character.UI;
+using System;
 
-public class WeaponHolder : MonoBehaviour
+
+namespace Character
 {
-    [Header("Weapon to Spawn"), SerializeField]
-    private GameObject weaponToSpawn;
-
-    [SerializeField] private Transform weaponSocketLocation;
-
-    // Start is called before the first frame update
-    void Start()
+    public class WeaponHolder : MonoBehaviour
     {
-      GameObject spawnweapon =  Instantiate(weaponToSpawn, weaponSocketLocation.position, weaponSocketLocation.rotation,weaponSocketLocation);
+        [Header("Weapon to Spawn"), SerializeField]
+        private GameObject weaponToSpawn;
 
-    }
+        [SerializeField] private Transform weaponSocketLocation;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        //compnents
+        private PlayerController PlayerController;
+        private Crosshair PlayerCrosshair;
+        private Animator PlayerAnimator;
+
+        //Ref
+        private Camera viewCamera;
+
+        private void Awake()
+        {
+
+            PlayerAnimator = GetComponent<Animator>();
+
+            PlayerController = GetComponent<PlayerController>();
+            if (PlayerController)
+            {
+                PlayerCrosshair = PlayerController.Crosshair;
+            }
+            viewCamera = Camera.main;
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            GameObject spawnweapon = Instantiate(weaponToSpawn, weaponSocketLocation.position, weaponSocketLocation.rotation, weaponSocketLocation);
+
+        }
+        public void OnLook(InputValue delta)
+        {
+            Vector2 independentMousePosition = viewCamera.ScreenToViewportPoint(PlayerCrosshair.CurrentAimPosition);
+           // Vector3 independentMousePosition = viewCamera.ScreenToViewportPoint(PlayerCrosshair.CurrentAimPosition);
+            Debug.Log(independentMousePosition);
+        }
+
+        //private void //OnEnable()
+        //{
+        //    base.OnEnable();
+        //    GameInput.PlayerActionMap.Look.performed += OnLook;
+        //}
+
+
+        //private void OnDisable()
+        //{
+        //    base.OnDisable();
+        //    GameInput.PlayerActionMap.Look.performed -= OnLook;
+        //}
     }
 }
