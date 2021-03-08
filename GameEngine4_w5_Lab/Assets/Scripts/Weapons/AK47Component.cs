b87d2da342a855a4c8fb40879cf1440cc778fabe
Talha991s <55731371+Talha991s@users.noreply.github.com>
2021-03-07 23:04:17 -0800

    Completing the Lab

--- GameEngine4_w5_Lab/Assets/Scripts/Weapons/AK47Component.cs
+++ GameEngine4_w5_Lab/Assets/Scripts/Weapons/AK47Component.cs
@@ -14,6 +14,11 @@ namespace Weapons
             if (WeaponStats.BulletsInClip > 0 && !Reloading && !WeaponHolder.Controller.IsRunning)
             {
                 base.FireWeapon();
+
+                if(!FiringEffect)
+                {
+                    FiringEffect = Instantiate(FiringAnimation, ParticleSpawnLocation).GetComponent<ParticleSystem>();
+                }
                 
                 Ray screenRay = MainCamera.ScreenPointToRay(new Vector3(CrosshairComponent.CurrentAimPosition.x,
                     CrosshairComponent.CurrentAimPosition.y, 0));
